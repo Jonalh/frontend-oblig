55e6fd86b89c8d08c8ae7d5bae3b59e7b7f2954c
Jonalh <jonalh@stud.ntnu.no>
2021-03-10 13:25:40 +0100

    snart ferdig nå

---
+++ js/studenterIKlasse.js
@@ -0,0 +1,15 @@
+let bigeomat = document.getElementById("BIGEOMAT");
+console.log(bigeomat);
+
+bigeomat.addEventListener("click", function() {
+    fetch('studenter.json').then(function (response){
+        return response.json();
+    }).then(function (obj) {
+        obj.forEach(element => {
+            let student = document.createElement("LI");
+            student.innerHTML = `Fornavn: ${element.fornavn} <br> Etternavn: ${element.etternavn} <br> Studieprogram: ${element.studieprogram} <br> Forkortelse: ${element.forkortelse} <br> --`;
+            let list = document.getElementsByClassName("studenter")[0];
+            list.appendChild(student);
+        });
+    });
+})
